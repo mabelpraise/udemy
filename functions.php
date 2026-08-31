f935376eab241f948a4dd5478ceea87420613767
Mabel Okeke <78964637+mabelpraise@users.noreply.github.com>
2026-08-31 03:45:10 +0100

    add hooks

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+//Variables
+
+//Includes
+
+//Hooks
+// Functions file
+
+function u_enqueue() {
+   // Code to enqueue styles and scripts
+}
+
+add_action('wp_enqueue_scripts', 'u_enqueue');
